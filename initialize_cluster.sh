37be8dc53dd7a2baca5c1b9b627dc976efe1725a
Fabijan Bajo <bajo09@gmail.com>
2019-06-26 05:03:08 +0200

    ensure let's encrypt can automatically issue certificates through ingress shim

--- initialize_cluster.sh
+++ initialize_cluster.sh
@@ -6,6 +6,14 @@ function install_tiller() {
   kubectl rollout status deployment tiller-deploy -n kube-system
 }
 
+function install_ingress() {
+  helm upgrade \
+    --install nginx-ingress \
+    --namespace kube-system \
+    -f ./nginx-ingress-overrides.yaml \
+    stable/nginx-ingress
+}
+
 function install_certmanager() {
   kubectl apply -f https://raw.githubusercontent.com/jetstack/cert-manager/release-0.8/deploy/manifests/00-crds.yaml
   helm repo add jetstack https://charts.jetstack.io
@@ -13,9 +21,14 @@ function install_certmanager() {
   helm upgrade \
     --install cert-manager \
     --namespace cert-manager \
+    -f ./cert-manager-overrides.yaml \
     jetstack/cert-manager
-  kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true"
+  kubectl label namespace cert-manager certmanager.k8s.io/disable-validation="true" \
+    --overwrite
+  kubectl apply -f ./staging-cluster-issuer.yaml
+  kubectl apply -f ./production-cluster-issuer.yaml
 }
 
 install_tiller
+install_ingress
 install_certmanager
