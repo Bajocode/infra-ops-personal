032b5048be71ebc2725c0b1719ee7b9fec84a6cc
Fabijan Bajo <bajo09@gmail.com>
2019-07-05 21:41:14 +0200

    Add deliver helmchart for easy CICD and update cluster init script

--- initialize_cluster.sh
+++ initialize_cluster.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+CI_NAMESPACE=tekton-pipelines
 
 function install_tiller() {
   kubectl apply -f ./tiller-rbac.yaml
@@ -31,15 +33,42 @@ function install_certmanager() {
 
 function install_tektoncd() {
   kubectl apply -f https://storage.googleapis.com/tekton-releases/latest/release.yaml
-  kubectl apply -f ./tektoncd/docker-gcr-secret.yaml
-  kubectl apply -f ./tektoncd/docker-sa.yaml
-  helm upgrade \
-    --install moby-buildkit \
-    --namespace tekton-pipelines \
-    ./moby-buildkit
+
+kubectl apply -f - <<EOF
+---
+apiVersion: rbac.authorization.k8s.io/v1beta1
+kind: ClusterRoleBinding
+metadata:
+  name: tekton-clusterrolebinding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: cluster-admin
+subjects:
+- kind: ServiceAccount
+  name: default
+  namespace: $CI_NAMESPACE
+EOF
+
+  local token_value=$(kubectl -n $CI_NAMESPACE get secret $(kubectl -n $CI_NAMESPACE get secret | grep ci-sa | awk '{print $1}') -o json | jq -r '.data.token'  | base64 -D)
+  local ca_value=kubectl config view --raw -o json | jq -r '.clusters[0].cluster."certificate-authority-data"' | tr -d '"' | base64 -D
+
+  kubectl create secret generic ci-cluster-secret \
+    --from-literal tokenKey=$token_value \
+    --from-literal caKey=$ca_value \
+    -n $CI_NAMESPACE --dry-run -o yaml | kubectl apply -f -
+}
+
+function create_ssh_secret() {
+  kubectl create secret generic git-secret \
+    -n kube-system \
+    --from-file=ssh=$HOME/.ssh/id_rsa \
+    --from-file=known_hosts=$HOME/.ssh/known_hosts \
+    --dry-run -o yaml | kubectl apply -f -
 }
 
 install_tiller
 install_ingress
 install_certmanager
 install_tektoncd
+create_ssh_secret
